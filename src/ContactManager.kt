c8af6cea80603294961082a92b0b2b884df80637
Bagus Erwanto <57589007+volumeee@users.noreply.github.com>
2024-04-17 10:09:47 +0700

    Add files via upload

---
+++ src/ContactManager.kt
@@ -0,0 +1,21 @@
+//by erdiansyah
+class ContactManager {
+    private val contacts: MutableList<Contact> = mutableListOf()
+
+    fun addContact(contact: Contact) {
+        contacts.add(contact)
+    }
+
+    fun removeContact(contact: Contact) {
+        contacts.remove(contact)
+    }
+
+    fun displayContacts() {
+        println("List of Contacts:")
+        contacts.forEachIndexed { index, contact ->
+            println("${index + 1}. ${contact.getName()} - ${contact.getPhoneNumber()} - ${contact.getEmail()}")
+        }
+    }
+}
+
+
